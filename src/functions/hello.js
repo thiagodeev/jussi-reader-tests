0cbbe0d64084889b9a6f4a6183b6e94b11ec2546
mariozuany <mario.zuanyneto@gmail.com>
2022-03-09 16:50:37 -0300

    test

--- src/functions/hello.js
+++ src/functions/hello.js
@@ -1,6 +1,48 @@
+
+const content = {
+  "news": [
+    {
+      "title": "Titulo da noticia",
+      "excerpt": "Resumo da notícia",
+      "image": "path/to/image.jpg",
+      "url": "path/to/article",
+      "categories": [
+        {
+          "name": "Categoria 1",
+          "slug": "categoria_1"
+        }
+      ]
+    },
+    {
+      "title": "Titulo da noticia",
+      "excerpt": "Resumo da notícia",
+      "image": null,
+      "url": "path/to/article",
+      "categories": []
+    },
+    {
+      "title": "Titulo da noticia",
+      "excerpt": "Resumo da notícia",
+      "image": "path/to/image.jpg",
+      "url": "path/to/article",
+      "categories": [
+        {
+          "name": "Categoria 1",
+          "slug": "categoria_1"
+        },
+        {
+          "name": "Categoria 2",
+          "slug": "categoria_2"
+        }
+      ]
+    }
+  ]
+}
+
+
 exports.handler = function (event, context, callback) {
   callback(null, {
     statusCode: 200,
-    body: "Hello, World",
+    body: content
   });
 };
