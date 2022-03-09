e82e4d99c124ff867c5426f2140e4d66307ba85c
mariozuany <mario.zuanyneto@gmail.com>
2022-03-09 17:01:18 -0300

    Headers config

--- src/functions/hello.js
+++ src/functions/hello.js
@@ -1,4 +1,12 @@
 
+
+const headers = {
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Headers': 'Content-Type',
+  'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE',
+  'Content-Type': 'application/json',
+};
+
 const content = {
   "news": [
     {
@@ -43,6 +51,7 @@ const content = {
 exports.handler = function (event, context, callback) {
   callback(null, {
     statusCode: 200,
+    headers,
     body: JSON.stringify(content)
   });
 };
