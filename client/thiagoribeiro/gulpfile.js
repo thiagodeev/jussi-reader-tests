0ea8c2a8790fa5b79af9c21f75c189d43d6ddf84
Thiago Ribeiro <thiago.ribeiro@jussi.com.br>
2022-03-30 19:00:42 -0300

    add js compiler task on gulpfile

--- client/thiagoribeiro/gulpfile.js
+++ client/thiagoribeiro/gulpfile.js
@@ -2,9 +2,11 @@
 
 const gulp = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
+const concat = require('gulp-concat');
 
 gulp.task("default", watch);
 gulp.task("sass", compileSass);
+gulp.task("js", compileJS);
 
 function compileSass(){
     return gulp
@@ -13,6 +15,17 @@ function compileSass(){
     .pipe(gulp.dest("src/"));
 };
 
+function compileJS(){
+    return gulp
+    .src([
+        "src/js/api.js",
+        "src/js/search.js"
+    ])
+    .pipe(concat("main.js"))
+    .pipe(gulp.dest("src/"));
+}
+
 function watch(){
     gulp.watch("src/scss/**/*.scss", compileSass);
+    gulp.watch("src/js/**/*.js", compileJS);
 }
---
+++ client/thiagoribeiro/src/main.js
@@ -0,0 +1,2 @@
+console.log("olá")
+console.log("mundooooo")
