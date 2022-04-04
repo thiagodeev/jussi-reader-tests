4608fd9cf398f3c0be86b22f6261af911fe4c019
Thiago Ribeiro <thiago.ribeiro@jussi.com.br>
2022-04-04 19:15:49 -0300

    improve the pagination

--- client/thiagoribeiro/src/js/script.js
+++ client/thiagoribeiro/src/js/script.js
@@ -1,33 +1,16 @@
 ;(async function main (){
-
   const numberOfNewsPerPage = 6;
   const newsFromAPI = await fetchAPI(newsURLs);
-  let currentPage = 0;
 
   let listOfAllAPINews = getAllNewsOf(newsFromAPI);
 
-  function divideTheArray(newsList, paginationNumber){
-    let tempArray = Array.from(newsList);
-    let organizedNewsList = [];
-    
-    while (tempArray.length) {
-        organizedNewsList.push(tempArray.splice(0, paginationNumber));
-    }
+  let allNewsInHTML = createsHTMLNewsFrom(listOfAllAPINews);
 
-    return organizedNewsList;
-  }
+  let organizedNews = divideTheArray(allNewsInHTML, numberOfNewsPerPage);
 
-  let organizedNews = divideTheArray(listOfAllAPINews, numberOfNewsPerPage);
- 
-  console.log(organizedNews)
+  renderNewsOnHTML(organizedNews);
 
-  renderNewsOnHTML(organizedNews, currentPage);
   writePagination(organizedNews);
-  
-  //get all the News in HTML format
-  // let allHTMLNews = Array.from(document.getElementsByClassName("newsList__news"));
-
-  // console.log(allHTMLNews)
 })();
 
 
