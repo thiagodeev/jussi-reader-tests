063b0902359e251fc64fdc0f54646a317ecac819
Thiago Ribeiro <thiago.ribeiro@jussi.com.br>
2022-04-04 17:12:36 -0300

    finished pagination feature. Working well

--- client/thiagoribeiro/src/js/pagination.js
+++ client/thiagoribeiro/src/js/pagination.js
@@ -3,33 +3,31 @@
     let paginationElement = document.getElementById("pagination");
     let paginationItem, paginationNumber;
 
-    //remove previous paginations. Faster to remove content than .innerHTML=""
+    //removes previous paginations. Faster to remove content than .innerHTML=""
     while (paginationElement.firstChild) {paginationElement.removeChild(paginationElement.lastChild);}
 
     for (let index=0; index < organizedNewsList.length; index++){
       paginationNumber = index + 1;
-
+      //creates the pagination item
       paginationItem = document.createElement("div");
       paginationItem.setAttribute("data-pagination", index);
       paginationItem.innerText = paginationNumber;
+      //adds the click event
       paginationItem.addEventListener("click", element => {
-        //faster to remove content than .innerHTML=""
-        while (newsListMainDiv.firstChild) {newsListMainDiv.removeChild(newsListMainDiv.lastChild);}
+        if(!element.currentTarget.classList.contains("active")){
+          //clears the newsList element
+          while (newsListMainDiv.firstChild) {newsListMainDiv.removeChild(newsListMainDiv.lastChild);}
+          //removes the class Active from all pagination items 
+          for (let i=0; i < paginationElement.children.length; i++){
+            paginationElement.children[i].removeAttribute('class');
+          }
 
-        renderNewsOnHTML(organizedNewsList, element.currentTarget.dataset.pagination);
+          renderNewsOnHTML(organizedNewsList, element.currentTarget.dataset.pagination);
+          //adds the Active class to current clicked element
+          element.currentTarget.classList.add("active");
+        }
       });
 
       paginationElement.appendChild(paginationItem);
     }
-
-    //set the first element as Active
-    //document.getElementById("pag-1").setAttribute("class", "is-active")
   }
-
-  // function changePage(){
-  //   //looks for pagination elements by ID and adds a clickEvent witch render news according to pagination ID 
-  //   Array.from(document.querySelectorAll("[id^='pag-']")).forEach(element => {
-  //     indexOfPage = element.id.replace("pag-", ""); //gets only the number on ID to use as Index
-  //     element.addEventListener("click", renderNewsOnHTML(organizedNews, indexOfPage));
-  //   });
-  // }
