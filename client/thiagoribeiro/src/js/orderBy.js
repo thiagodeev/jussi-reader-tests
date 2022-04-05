a11ff838ef68a49b6e0c6ec5210b26e5e4696c54
Thiago Ribeiro <thiago.ribeiro@jussi.com.br>
2022-04-05 14:02:31 -0300

    orderBy separated functions

--- client/thiagoribeiro/src/js/orderBy.js
+++ client/thiagoribeiro/src/js/orderBy.js
@@ -1,11 +1,42 @@
+//********** title **********//
 function orderByTitle(newsList){
   let ordenedList = Array.from(newsList).sort((a, b) => a.title < b.title ? -1 : 1);
 
   return ordenedList;
 }
 
+function renderHTMLListOrderedByTitle(elementToRender, renderfunction){
+  let orderByTitleElement = document.getElementById("orderByTitle");
+  orderByTitleElement.addEventListener("click", element => {
+    document.getElementById("orderByDate").removeAttribute('class');
+    
+    if(!element.currentTarget.classList.contains("active")){
+      renderfunction(elementToRender);
+      
+      element.currentTarget.classList.add("active");
+    };
+  });
+};
+
+//********** date **********//
 function orderByDate(newsList){
   let ordenedList = Array.from(newsList).sort((a, b) => a.date_published > b.date_published ? -1 : 1);
 
   return ordenedList;
 }
+
+function renderHTMLListOrderedByDate(elementToRender, renderfunction){
+  let orderByDateElement = document.getElementById("orderByDate");
+  orderByDateElement.addEventListener("click", element => {
+    document.getElementById("orderByTitle").removeAttribute('class');
+
+    if(!element.currentTarget.classList.contains("active")){
+      renderfunction(elementToRender);
+      
+      element.currentTarget.classList.add("active");
+    };
+  });
+};
+
+// //********** title and date **********//
+// function renderOrderedBy(elementToRender)
--- client/thiagoribeiro/src/js/script.js
+++ client/thiagoribeiro/src/js/script.js
@@ -21,49 +21,15 @@
   renderNews(organizedNews);
 
   //********** orderBy **********//
-
-  function renderOrdenedByTitle(elementToRender){
-
-  }
-
   let newsOrganizedByTitle = orderByTitle(allAPINews);
   let HTMLnewsOrganizedByTitle = formatNewsElement(newsOrganizedByTitle);
-  let orderByTitleElement = document.getElementById("orderByTitle");
-  orderByTitleElement.addEventListener("click", element => {
-    document.getElementById("orderByDate").removeAttribute('class');
-    
-    if(!element.currentTarget.classList.contains("active")){
-      renderNews(HTMLnewsOrganizedByTitle);
-      
-      element.currentTarget.classList.add("active");
-    };
-  });
 
-  // let newsOrganizedByTitle = orderByTitle(allAPINews);
-  // let HTMLnewsOrganizedByTitle = formatNewsElement(newsOrganizedByTitle);
-  // let orderByTitleElement = document.getElementById("orderByTitle");
-  // orderByTitleElement.addEventListener("click", element => {
-  //   document.getElementById("orderByDate").removeAttribute('class');
-    
-  //   if(!element.currentTarget.classList.contains("active")){
-  //     renderNews(HTMLnewsOrganizedByTitle);
-      
-  //     element.currentTarget.classList.add("active");
-  //   };
-  // });
-
+  renderHTMLListOrderedByTitle(HTMLnewsOrganizedByTitle, renderNews);
+  
   let newsOrganizedByDate = orderByDate(allAPINews);
   let HTMLnewsOrganizedByDate = formatNewsElement(newsOrganizedByDate);
-  let orderByDateElement = document.getElementById("orderByDate");
-  orderByDateElement.addEventListener("click", element => {
-    document.getElementById("orderByTitle").removeAttribute('class');
 
-    if(!element.currentTarget.classList.contains("active")){
-      renderNews(HTMLnewsOrganizedByDate);
-      
-      element.currentTarget.classList.add("active");
-    };
-  });
+  renderHTMLListOrderedByDate(HTMLnewsOrganizedByDate, renderNews);
 
 })();
 
