0328669a2ce8b7a38b6dad652718328b0f2795a0
Thiago Ribeiro <thiago.ribeiro@jussi.com.br>
2022-04-11 23:42:03 -0300

    make some changes

--- client/thiagoribeiro/src/js/renderFunctions.js
+++ client/thiagoribeiro/src/js/renderFunctions.js
@@ -21,6 +21,36 @@ function divideTheArray(arrayToDivide){
   return organizedNewsList;
 };
 
+function makeFavoriteElement(){
+  let favoriteElement = document.createElement("span");
+  favoriteElement.classList.add("newsList__news__favorite");
+  
+  let defaultPath = "M30.4 16q1.5-1.3 2-2.6t.6-3q0-1.4-.7-3T30.6 5q-1.4-1.2-2.4-1.6T25.8 3q-1.5 0-3 .6t-2.6 2l-2 2-2.3-2q-1.8-1.4-3-2T10.2 3t-2.6.4T5.3 5q-1 .7-1.6 2.4t-.7 3q0 1.4.6 3T5.4 16L18 28l12.4-12zM0 10.5q0-1.7.8-4t2.6-3.8Q5 1.2 6.7.7t3.6-.7q2 0 3.8.8t4 2.7q2-2 4-2.7t4-.8 3.4.6 3.3 2Q34.3 4 35 6.3t1 4-.6 4-3 4L18 32 3.4 18.2Q1 16 .4 13.7T0 10.4z";
+  let clickedPath = "M0 10.4q0-1.7.8-4t2.6-3.8Q5 1.2 6.7.6t3.6-.6q2 0 3.8.8t4 2.7q2-2 4-2.7t4-.8 3.4.6 3.3 2Q34.3 4 35 6.3t1 4-.6 4-3 4L18 32 3.4 18.2Q1.8 16.7 1 14.7t-1-4.3z";
+
+  let SVGPath = document.createElement("path");
+  SVGPath.setAttribute("d", defaultPath);
+  let SVGElement = document.createElement("svg");
+  SVGElement.setAttribute("viewBox", "0 0 36 32");
+
+  SVGElement.appendChild(SVGPath);
+  favoriteElement.appendChild(SVGElement);
+
+  favoriteElement.addEventListener("click", element => {
+    element.currentTarget.classList.toggle("favorite");
+
+    if(element.currentTarget.classList.contains("favorite")){
+      let pathElement = element.currentTarget.firstChild.firstChild;
+      pathElement.setAttribute("d", clickedPath);
+    } else {
+      let pathElement = element.currentTarget.firstChild.firstChild;
+      pathElement.setAttribute("d", defaultPath);
+    }
+  });
+
+  return favoriteElement;
+}
+
 function returnAListOfCategories(listOfCategories){
   let divCategoriesList = document.createElement("div");
   divCategoriesList.classList.add("newsList__news__categories");
@@ -78,16 +108,19 @@ function newsTemplate(newsList){
   })(newsList.excerpt, 75);
 
   let divCategoriesList = returnAListOfCategories(newsList.categories);
+  let favoriteElement = makeFavoriteElement();
+
 
   element = document.createElement("div");
   element.classList.add("newsList__news");
-  element.innerHTML = `      
+  element.innerHTML = `  
     <img class="newsList__news__image" src="${newsList.image}" onerror="this.onerror=null; this.src='assets/image-placeholder.jpg'" alt="">
     <h2 class="newsList__news__title">${newsList.title}</h2>
     <p class="newsList__news__date_published">${date.toLocaleDateString("pt-BR", options)}</p>
     <p class="newsList__news__excerpt">${excerpt}</p>
   `;
   element.appendChild(divCategoriesList);
+  element.appendChild(favoriteElement);
 
   element.addEventListener("click", element => {
     if(element.target.localName != "span"){
